4510f5ea8549c27554e376c4cffac9b699f3c270
StuuGraham <stuartgraham7@hotmail.co.uk>
2021-01-25 16:46:47 +0000

    Added a line break in UpcomingFilms.js to seperate it from the list in FilmBox.js

--- src/components/UpcomingFilms.js
+++ src/components/UpcomingFilms.js
@@ -3,6 +3,7 @@ import React, { Fragment } from 'react';
 const UpcomingFilms = () => {
     return(
         <Fragment>
+            <br></br>
             <a href="https://www.imdb.com/calendar/?region=gb">
                 <div>View More Upcoming Releases Here!</div>
             </a>
